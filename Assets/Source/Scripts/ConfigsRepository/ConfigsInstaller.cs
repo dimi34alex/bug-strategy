fa33e3c7431d8d09503c81d6b74926bc526ab2ca
Razrob <razrobs@yandex.ru>
2022-10-16 14:00:45 +0300

    configs repository

---
+++ Assets/Source/Scripts/ConfigsRepository/ConfigsInstaller.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using Zenject;
+
+public class ConfigsInstaller : MonoInstaller
+{
+    [SerializeField] private ConfigsRepository _configsRepository;
+
+    public override void InstallBindings()
+    {
+        Container.BindInstance(_configsRepository).AsSingle();
+
+        foreach (ISingleConfig config in _configsRepository.Configs)
+            Container.Bind(config.GetType()).FromInstance(config).AsSingle();
+    }
+}
