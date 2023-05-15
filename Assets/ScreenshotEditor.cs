3f627ead6e6a962722a46411cdd22117a6ded541
Razrob <razrobs@yandex.ru>
2023-05-15 20:35:01 +0300

    day night change

---
+++ Assets/ScreenshotEditor.cs
@@ -0,0 +1,41 @@
+﻿#if UNITY_EDITOR
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+    public static class ScreenshotEditor
+    {
+        [MenuItem("ScreenshotEditor/Take Screenshot %g")]
+        private static void TakeScreenshot()
+        {
+            const int superSize = 1;
+            
+            const string extension = ".png";
+            const string folder = "Screenshots";
+            
+            var date = System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss");
+            var path = Application.dataPath.Replace("Assets", "");
+            
+            date += extension;
+
+            path = Path.Combine(path, folder);
+            
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            path = Path.Combine(path, date);
+
+            if (File.Exists(path))
+            {
+                Debug.LogWarning($"File in path:{path} exist");
+                return;
+            }
+            
+            ScreenCapture.CaptureScreenshot(path, superSize);
+
+            Debug.Log($"<color=green>Screenshot created in {path}</color>");
+        }
+    }
+#endif
--- Assets/Source/Scripts/DayChanger/DayChanger.cs
+++ Assets/Source/Scripts/DayChanger/DayChanger.cs
@@ -1,18 +1,67 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
 
 public class DayChanger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float _cycleDuration;
+    [SerializeField] private PostProcessVolume _volume;
+    [SerializeField] private AnimationCurve _dayCurve;
+
+    private const float MAX_VIGNETTE_INTENSITY = 0.6F;
+    private const float MIN_POST_EXPOSURE = -1.8F;
+
+    private Vignette _vignette;
+    private ColorGrading _colorGrading;
+
+    private float _currentDayTime = 0.5f; // [0; 1)
+
+    public DayChanger Instance { get; private set; }
+
+    private void OnValidate()
     {
-        
+        if (!_volume)
+            _volume = GetComponent<PostProcessVolume>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        Instance = this;
+        _vignette = _volume.profile.GetSetting<Vignette>();
+        _colorGrading = _volume.profile.GetSetting<ColorGrading>();
+    }
+
+    private void Update()
+    {
+        IncreaseDayTime();
+
+        float nightIntensity = (Mathf.Abs(1f - _dayCurve.Evaluate(_currentDayTime)));
+
+        _vignette.intensity.Override(nightIntensity * MAX_VIGNETTE_INTENSITY);
+        _colorGrading.postExposure.Override(nightIntensity * MIN_POST_EXPOSURE);
+    }
+
+    private void IncreaseDayTime()
+    {
+        _currentDayTime = Mathf.Sin((Time.time * (Mathf.PI * 2f)) / (_cycleDuration * 2f));
+        _currentDayTime = _currentDayTime / 2f + 0.5f;
+    }
+
+    public TimeSpan GetTime()
+    {
+        float remainingTime = _currentDayTime;
+
+        int hours = (int)(remainingTime * 24f);
+        remainingTime -= hours;
+
+        int minutes = (int)(remainingTime * 60f);
+        remainingTime -= minutes;
+
+        int seconds = (int)(remainingTime * 60f);
+
+        return new TimeSpan(hours, minutes, seconds);
     }
 }
