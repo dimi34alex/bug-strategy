30d88222b8e97d19f61713b6708b59ceb804f552
AndreyLua <panzar.egorov1@yandex.ru>
2023-05-18 01:03:19 +0300

    reworked resourceStorage update (optimization)

--- Assets/Source/Scripts/ResourcesSystem/ResourceGlobalStorage.cs
+++ Assets/Source/Scripts/ResourcesSystem/ResourceGlobalStorage.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,7 +7,9 @@ public class ResourceGlobalStorage : MonoBehaviour
 {
     private static ResourceRepository _resourceRepository;
     public ResourceConfig[] resourceConfigs;
-    
+
+    public static event Action ResourceChanged;
+
     void Awake()
     {
         _resourceRepository = new ResourceRepository(resourceConfigs);
@@ -14,6 +17,27 @@ public class ResourceGlobalStorage : MonoBehaviour
         _resourceRepository.CreateResource(ResourceID.Bees_Wax,10, 10);
         _resourceRepository.CreateResource(ResourceID.Housing,10, 10);
         _resourceRepository.CreateResource(ResourceID.Honey,10, 10);
+
+        _resourceRepository.OnResourceAdd += OnResourceAdded;
+
+        foreach (var element in _resourceRepository.Resources)
+            element.Value.OnChange += OnResourceChanged;
+
+    }
+
+    private void Start()
+    {
+        OnResourceChanged();
+    }
+
+    private void OnResourceChanged()
+    {
+        ResourceChanged?.Invoke();
+    }
+
+    private void OnResourceAdded(ResourceBase resourceBase)
+    {
+        ResourceChanged?.Invoke();
     }
 
     public static void ChangeCapacity(ResourceID resourceID, float capacity)
