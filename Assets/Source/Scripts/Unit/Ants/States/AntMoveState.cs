898fd7632124d257240617a4f119343de1da9145
denfranke <33222627+denfranke@users.noreply.github.com>
2024-09-28 14:37:05 +0300

    fix chech _ant in AntMoveState

--- Assets/Source/Scripts/Unit/Ants/States/AntMoveState.cs
+++ Assets/Source/Scripts/Unit/Ants/States/AntMoveState.cs
@@ -10,65 +10,72 @@ namespace BugStrategy.Unit.Ants
         public override EntityStateID EntityStateID => EntityStateID.Move;
 
         private const float DistanceBuffer = 0.1f;
-        
+
         private readonly AntBase _ant;
 
         private event Action UpdateEvent;
         public override event Action StateExecuted;
-        
-        public AntMoveState(AntBase ant)
+
+        public AntMoveState (AntBase ant)
         {
             _ant = ant;
         }
-        
-        public override void OnStateEnter()
+
+        public override void OnStateEnter ()
         {
             if(_ant != null)
             {
                 _ant.SetDestination(_ant.TargetMovePosition);
                 _ant.OnTargetMovePositionChange += UpdateDestinationPosition;
+
+
+                if(_ant.CurrentPathData.Target.IsAnyNull())
+                    UpdateEvent += ManualCheckDistance;
+                else
+                    _ant.CurrentProfession.OnEnterInZone += CheckTargetDistance;
             }
-
-            if (_ant.CurrentPathData.Target.IsAnyNull())
-                UpdateEvent += ManualCheckDistance;
-            else
-                _ant.CurrentProfession.OnEnterInZone += CheckTargetDistance;
         }
 
-        public override void OnStateExit()
+        public override void OnStateExit ()
         {
-            _ant.SetDestination(_ant.Transform.position);
-            _ant.OnTargetMovePositionChange -= UpdateDestinationPosition;
-            
-            UpdateEvent -= ManualCheckDistance;
-            _ant.CurrentProfession.OnEnterInZone -= CheckTargetDistance;
+            if(_ant != null)
+            {
+                _ant.SetDestination(_ant.Transform.position);
+                _ant.OnTargetMovePositionChange -= UpdateDestinationPosition;
+
+                UpdateEvent -= ManualCheckDistance;
+                _ant.CurrentProfession.OnEnterInZone -= CheckTargetDistance;
+            }
         }
 
-        public override void OnUpdate() => UpdateEvent?.Invoke();
+        public override void OnUpdate () => UpdateEvent?.Invoke();
 
-        private void UpdateDestinationPosition()
+        private void UpdateDestinationPosition ()
         {
-            _ant.SetDestination(_ant.TargetMovePosition);
-            
-            UpdateEvent -= ManualCheckDistance;
-            _ant.CurrentProfession.OnEnterInZone -= CheckTargetDistance;
+            if(_ant != null)
+            {
+                _ant.SetDestination(_ant.TargetMovePosition);
 
-            if (_ant.CurrentPathData.Target.IsAnyNull())
-                UpdateEvent += ManualCheckDistance;
-            else
-                _ant.CurrentProfession.OnEnterInZone += CheckTargetDistance;
+                UpdateEvent -= ManualCheckDistance;
+                _ant.CurrentProfession.OnEnterInZone -= CheckTargetDistance;
+
+                if(_ant.CurrentPathData.Target.IsAnyNull())
+                    UpdateEvent += ManualCheckDistance;
+                else
+                    _ant.CurrentProfession.OnEnterInZone += CheckTargetDistance;
+            }
         }
 
-        private void ManualCheckDistance()
+        private void ManualCheckDistance ()
         {
             if(Vector3.Distance(_ant.Transform.position, _ant.TargetMovePosition) < DistanceBuffer)
                 // _ant.HandleGiveOrder(_ant.CurrentPathData.Target, _ant.CurrentPathData.PathType);
                 StateExecuted?.Invoke();
         }
-        
-        private void CheckTargetDistance()
+
+        private void CheckTargetDistance ()
         {
-            if (_ant.CurrentProfession.OrderValidatorBase.CheckDistance(_ant.CurrentPathData))
+            if(_ant.CurrentProfession.OrderValidatorBase.CheckDistance(_ant.CurrentPathData))
                 // _ant.HandleGiveOrder(_ant.CurrentPathData.Target, _ant.CurrentPathData.PathType);
                 StateExecuted?.Invoke();
         }
