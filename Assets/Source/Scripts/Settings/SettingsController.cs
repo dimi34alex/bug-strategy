c3f669a468107a50f8495b436529086fb37f18dd
FandXOfficial <fc392157@gmail.com>
2024-11-17 14:43:40 +0300

    мини правки
    - исправлена ошибка при переходе в настройки
    - ещё по мелочи

--- Assets/Source/Scripts/Settings/SettingsController.cs
+++ Assets/Source/Scripts/Settings/SettingsController.cs
@@ -11,12 +11,10 @@ namespace BugStrategy
 {
     public class SettingsController : MonoBehaviour
     {
-        // Dropdowns для настроек экрана
-        public TMP_Dropdown screenModeDropdown;
-        public TMP_Dropdown resolutionDropdown;
-        public TMP_Dropdown fpsDropdown;
+        [SerializeField] private TMP_Dropdown screenModeDropdown;
+        [SerializeField] private TMP_Dropdown resolutionDropdown;
+        [SerializeField] private TMP_Dropdown fpsDropdown;
 
-        // Массивы разрешений и FPS
         private Resolution[] resolutions;
         private int[] fpsOptions = { 30, 60, 120, 240 };
 
@@ -36,37 +34,31 @@ namespace BugStrategy
                 Resolution res = resolutions[i];
                 resolutionDropdown.options.Add(new TMP_Dropdown.OptionData(res.width + " x " + res.height));
 
-                // Проверяем, совпадает ли разрешение с текущим
                 if (res.width == Screen.currentResolution.width && res.height == Screen.currentResolution.height)
                 {
                     currentResolutionIndex = i;
                 }
             }
 
-            // Устанавливаем текущее разрешение
             resolutionDropdown.value = currentResolutionIndex;
             resolutionDropdown.RefreshShownValue();
 
-            // Добавляем варианты FPS
-            fpsDropdown.ClearOptions();
+            //fpsDropdown.ClearOptions();
             foreach (var fps in fpsOptions)
             {
                 fpsDropdown.options.Add(new TMP_Dropdown.OptionData(fps + " FPS"));
             }
 
-            // Устанавливаем текущее значение для режима экрана (0 - окно, 1 - полный экран)
             screenModeDropdown.value = Screen.fullScreen ? 1 : 0;
         }
 
         public void ApplySettings()
         {
-            // Применяем разрешение и режим экрана
             Resolution selectedResolution = resolutions[resolutionDropdown.value];
             bool isFullScreen = screenModeDropdown.value == 1; // 0 - оконный, 1 - полный экран
 
             Screen.SetResolution(selectedResolution.width, selectedResolution.height, isFullScreen);
 
-            // Применяем FPS
             int selectedFps = fpsOptions[fpsDropdown.value];
             Application.targetFrameRate = selectedFps;
         }
