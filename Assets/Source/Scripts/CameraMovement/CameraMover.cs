d308c60ca2aa04b4f167546d2667fb6552ab2825
denfranke <33222627+denfranke@users.noreply.github.com>
2024-11-19 00:17:06 +0300

    подправил

--- Assets/Source/Scripts/CameraMovement/CameraMover.cs
+++ Assets/Source/Scripts/CameraMovement/CameraMover.cs
@@ -13,8 +13,15 @@ namespace BugStrategy.CameraMovement
         [SerializeField] private Camera _camera;
         [SerializeField] private GameObject field;
 
+        [SerializeField] private float CameraMoveSpeed;
+        [SerializeField] private float PercentageOfScreen;
+
+        private float SizeBorderToMove;
+        private float xDist, yDist;
+
         [Inject] private readonly IInputProvider _inputProvider;
 
+        private Vector2 delta;
         private Vector3[] _bounds;
         private Vector3 _startPosition;
         private Vector3 _targetPosition;
@@ -24,26 +31,66 @@ namespace BugStrategy.CameraMovement
             _bounds = new Vector3[2];
             _bounds[0] = new Vector3(field.transform.localScale.x * -5f, -100f, field.transform.localScale.z * -5f);
             _bounds[1] = new Vector3(field.transform.localScale.x * 5f, 100f, field.transform.localScale.z * 5f);
+            SizeBorderToMove = Mathf.Min(Screen.width, Screen.height) * (PercentageOfScreen / 100);
         }
 
         private void LateUpdate ()
-            => Move();
+        {
+            MoveByCursor();
+            Move();
+        }
 
         private void Move ()
         {
+            if(CheckIfCursorInBorderZone())
+                return;
+
             _targetPosition.y = transform.position.y;
 
             if(_inputProvider.ScrollDown)
                 _startPosition = transform.position + _camera.ScreenToWorldPoint(_inputProvider.MousePosition).XZ();
 
             if(_inputProvider.ScrollHold)
+            {
                 _targetPosition = _startPosition - _camera.ScreenToWorldPoint(_inputProvider.MousePosition).XZ();
 
-            Vector3 position = Vector3.Lerp(transform.position, _targetPosition, Time.deltaTime * _moveSpeed);
+                Vector3 position = Vector3.Lerp(transform.position, _targetPosition, Time.deltaTime * _moveSpeed);
 
-            position = position.Clamp(_bounds[0], _bounds[1]);
+                position = position.Clamp(_bounds[0], _bounds[1]);
 
-            transform.position = position;
+                transform.position = position;
+            }
+        }
+
+        private void MoveByCursor ()
+        {
+            if(_inputProvider.ScrollDown || _inputProvider.ScrollHold)
+                return;
+
+            if(CheckIfCursorInBorderZone())
+            {
+                delta = delta.normalized;
+                delta *= Mathf.Clamp01(1 - Mathf.Min(xDist, yDist) / SizeBorderToMove);
+
+                transform.Translate(delta * CameraMoveSpeed * Time.deltaTime, Space.Self);
+                transform.position = transform.position.Clamp(_bounds[0], _bounds[1]);
+            }
+        }
+
+        private bool CheckIfCursorInBorderZone ()
+        {
+            Vector2 mousePos = Input.mousePosition;
+            mousePos.x /= Screen.width;
+            mousePos.y /= Screen.height;
+
+            delta = mousePos - new Vector2(0.5f, 0.5f);
+
+            xDist = Screen.width * (0.5f - Mathf.Abs(delta.x));
+            yDist = Screen.height * (0.5f - Mathf.Abs(delta.y));
+
+            if(xDist < SizeBorderToMove || yDist < SizeBorderToMove)
+                return true;
+            return false;
         }
     }
 }
