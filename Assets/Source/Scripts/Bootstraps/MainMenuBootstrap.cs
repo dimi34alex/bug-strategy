9d277a09132ad74cd2e03e1798126ad3b72468a5
Rodion <workavast@yandex.ru>
2024-12-15 23:03:20 +0300

    improve loading screen in the main menu

--- Assets/Source/Scripts/Bootstraps/MainMenuBootstrap.cs
+++ Assets/Source/Scripts/Bootstraps/MainMenuBootstrap.cs
@@ -1,17 +1,20 @@
 using BugStrategy.ScenesLoading;
 using CycleFramework.Execute;
+using UnityEngine;
 using Zenject;
 
 namespace BugStrategy.Bootstraps
 {
     public class MainMenuBootstrap : CycleInitializerBase
     {
-        [Inject] private ISceneLoader _sceneLoader;
+        [SerializeField] private bool hideLoadingScreenInstantly;
+        
+        [Inject] private readonly ISceneLoader _sceneLoader;
 
         protected override void OnStartInit()
         {
             base.OnStartInit();
-            _sceneLoader.Initialize(false);
+            _sceneLoader.Initialize(hideLoadingScreenInstantly);
         }
     }
 }
