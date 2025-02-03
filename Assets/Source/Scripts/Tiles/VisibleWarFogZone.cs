fd65499d0f44dc77e338356290bcf916d68d88d8
Rodion <workavast@yandex.ru>
2025-02-03 20:19:04 +0300

    remove unused VisibleWarFogZone, fixes

--- Assets/Source/Scripts/Tiles/VisibleWarFogZone.cs
+++ Assets/Source/Scripts/Tiles/VisibleWarFogZone.cs
@@ -1,17 +1,30 @@
 using System;
-using BugStrategy.Constructions;
 using BugStrategy.Missions;
 using BugStrategy.Trigger;
-using BugStrategy.Unit;
 using CycleFramework.Extensions;
-using UnityEngine;
 using Zenject;
 
 namespace BugStrategy.Tiles
 {
-    public class VisibleWarFogZone : TriggerZone
+    public abstract class VisibleWarFogZone : TriggerZone
     {
+        [Inject] private readonly MissionData _missionData;
+
         protected override Func<ITriggerable, bool> EnteredComponentIsSuitable => t => t is Tile;
-        protected override bool _refreshEnteredComponentsAfterExit => false;      
+        protected override bool _refreshEnteredComponentsAfterExit => false;
+
+        protected abstract AffiliationEnum GetAffiliation();
+        
+        protected override void OnEnter (ITriggerable component)
+        {
+            if(GetAffiliation() == _missionData.PlayerAffiliation)
+                component.Cast<Tile>().AddWatcher();
+        } 
+        
+        protected override void OnExit (ITriggerable component)
+        {
+            if(GetAffiliation() == _missionData.PlayerAffiliation)
+                component.Cast<Tile>().RemoveWatcher();
+        }
     }
 }
