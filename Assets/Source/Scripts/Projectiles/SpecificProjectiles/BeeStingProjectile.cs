4929b929494fa65d1560dbaea8a99fd13bcd4072
denfranke <33222627+denfranke@users.noreply.github.com>
2025-02-12 16:02:22 +0300

    Добавлено вращение прожектайлов в сторону цели

--- Assets/Source/Scripts/Projectiles/SpecificProjectiles/BeeStingProjectile.cs
+++ Assets/Source/Scripts/Projectiles/SpecificProjectiles/BeeStingProjectile.cs
@@ -1,3 +1,8 @@
+using BugStrategy.EntityState;
+using BugStrategy.Unit;
+using BugStrategy.Unit.Ants;
+using UnityEngine;
+
 namespace BugStrategy.Projectiles
 {
     public class BeeStingProjectile : ProjectileBase
