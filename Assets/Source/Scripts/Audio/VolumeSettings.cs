db70d1cc06ef4884c61aaac5543473db50a85f68
Rodion <workavast@yandex.ru>
2025-03-22 18:29:49 +0300

    settings fixes and improves

--- Assets/Source/Scripts/Audio/VolumeSettings.cs
+++ Assets/Source/Scripts/Audio/VolumeSettings.cs
@@ -12,12 +12,16 @@ namespace BugStrategy.Audio
         private float _prevMasterVolume;
         private float _prevMusicVolume;
         private float _prevEffectsVolume;
+
+        private const float DefaultMasterVolume = 1f;
+        private const float DefaultMusicVolume = 1f;
+        private const float DefaultEffectsVolume = 1f;
         
         public VolumeSettings()
         {
-            Master = 1;
-            MusicVolume = 1f;
-            EffectsVolume = 1f;
+            Master = DefaultMasterVolume;
+            MusicVolume = DefaultMusicVolume;
+            EffectsVolume = DefaultEffectsVolume;
         }
     
         public void ChangeMasterVolume(float newVolume) 
@@ -46,10 +50,18 @@ namespace BugStrategy.Audio
 
         public void Revert()
         {
-            IsChanged = false;
+            IsChanged = true;
             Master = _prevMasterVolume;
             MusicVolume = _prevMusicVolume;
             EffectsVolume = _prevEffectsVolume;
+        }
+        
+        public void ResetToDefault()
+        {
+            IsChanged = true;
+            Master = DefaultMasterVolume;
+            MusicVolume = DefaultMusicVolume;
+            EffectsVolume = DefaultEffectsVolume;
         }
         
         public void ResetChangedMarker() 
